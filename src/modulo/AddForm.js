01c9428dc8107b10273641afcd90140af195594d
Nataly-M-M <138437606+Nataly-M-M@users.noreply.github.com>
2023-11-23 00:40:18 -0600

    Componentes del form

--- src/modulo/AddForm.js
+++ src/modulo/AddForm.js
@@ -5,19 +5,16 @@ import './app.css'
 
 export default function AddForm() {
     let navigate = useNavigate()
-
     const [form, setForm] = useState({
         descriptionForm: "",
         isEnable: true,
     })
-
     const { descriptionForm, isEnable } = form
-
     const onInputChange = (e) => {
         setForm({ ...form, [e.target.name]: e.target.value })
     }
 
-
+    let IdForm = 0;
     //Los commponentes de cada formulario....................................................................
     const [components, setComponents] = useState([]);// el vector de componentes de cada vector
     const [component, setComponent] = useState({
@@ -29,10 +26,10 @@ export default function AddForm() {
         placeHolder: '',
         nameDescription: '',
         formId: '',
-        componentNameId:'',
+        componentNameId: '',
     })
 
-    const {typeComponentId,text,value,isVisible,isEnable1,placeHolder,nameDescription,formId,componentNameId } = component
+    const { typeComponentId, text, value, isVisible, isEnable1, placeHolder, nameDescription, formId, componentNameId } = component
 
     const onInputChangeComponent = (e) => {
         setComponent({ ...component, [e.target.name]: e.target.value })
@@ -83,12 +80,42 @@ export default function AddForm() {
             isEnable: JSON.parse(isEnable),
         }
         console.log(formData)
-        await axios.post("https://localhost:7179/api/Forms", formData)
-        navigate("/")
+        const response = await axios.post("https://localhost:7179/api/Forms", formData)
+        IdForm = response.data.id;
+        console.log(IdForm);
+       
+
+    }
+
+    //agregar los componentes de cada formulario................
+    const onSubmit2 = async (e) => {
+        e.preventDefault()
+        const email = 'isaacfallasv@gmail.com';
+        const password = 'ifv123';
+        const credentials = btoa(`${email}:${password}`);
+
+        const headers = {
+            Authorization: `Basic ${credentials}`,
+            'Content-Type': 'application/json',
+        };
+        axios.defaults.headers.common = headers;
 
 
-        //agregar los componentes de cada formulario................
-        
+        const componentData = {
+            typeComponentId: parseInt(typeComponentId),
+            text: text,
+            value: value,
+            isVisible: true,
+            isEnable: true,
+            placeHolder: placeHolder,
+            nameDescription: nameDescription,
+            formId: parseInt(IdForm),
+            componentNameId: componentNameId,
+
+        }
+        console.log(componentData)
+        const response = await axios.post("https://localhost:7179/api/Components", componentData)
+      
     }
 
     return (
@@ -134,21 +161,84 @@ export default function AddForm() {
 
             <div className='components'>
                 <h2>COMPONENTES</h2>
-                <select
-                    className="form-control"
-                    name="typeComponentId"
-                    value={typeComponentId}
-                    onChange={(e) => onInputChangeComponent(e)}
-                >
-                    <option value="">Selecciona un componente</option>
-                    {typeComponents.map((comp) => (
-                        <option key={comp.id} value={comp.id}>
-                            {comp.nameComponent}
-                        </option>
-                    ))}
-                </select>
-            </div>
 
-        </div>
+                <form onSubmit={onSubmit2}>
+                    <div>
+                        <select
+                            className="form-control"
+                            name="typeComponentId"
+                            value={typeComponentId}
+                            onChange={(e) => onInputChangeComponent(e)}
+                        >
+                            <option value="">Selecciona un componente</option>
+                            {typeComponents.map((comp) => (
+                                <option key={comp.id} value={comp.id}>
+                                    {comp.nameComponent}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="text" className="form-label">Texto</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="text"
+                            value={text}
+                            onChange={(e) => onInputChangeComponent(e)}
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="value" className="form-label">Valor</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="value"
+                            value={value}
+                            onChange={(e) => onInputChangeComponent(e)}
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="placeHolder" className="form-label">Marcador de Posición</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="placeHolder"
+                            value={placeHolder}
+                            onChange={(e) => onInputChangeComponent(e)}
+                        />
+                    </div>
+
+                    <div className="mb-3">
+                        <label htmlFor="nameDescription" className="form-label"> NameDescripción</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="nameDescription"
+                            value={nameDescription}
+                            onChange={(e) => onInputChangeComponent(e)}
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="componentNameId" className="form-label">Nombre del Componente</label>
+                        <input
+                            type="text"
+                            className="form-control"
+                            name="componentNameId"
+                            value={componentNameId}
+                            onChange={(e) => onInputChangeComponent(e)}
+                        />
+                    </div>
+
+
+                    <button type='submit' className='btn btn-outline-primary'>Guardar componente</button>
+                </form>
+
+            </div >
+
+        </div >
     )
 }
